fd7cc9b8ec03c24cf95c48f610053b06e773400d
Darrick Fauvel <darrick.fauvel@gmail.com>
2024-03-11 18:16:38 -0400

    Complete challenge

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -1,1 +1,17 @@
+// CHALLENGE: Functions - Using Objects as Function Parameters
+function processData(
+  input: string | number,
+  config: { reverse: boolean } = { reverse: false }
+) {
+  if (typeof input === "number") {
+    return input * input
+  } else {
+    return config.reverse
+      ? input.toUpperCase().split("").reverse().join("")
+      : input.toUpperCase()
+  }
+}
 
+console.log(processData(20))
+console.log(processData("hello"))
+console.log(processData("hello", { reverse: true }))
